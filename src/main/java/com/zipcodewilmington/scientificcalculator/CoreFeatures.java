09b778375b14335782201150e5255a3b319d085e
jenn-182 <jenniferstaci182@gmail.com>
2025-07-12 14:55:37 -0400

    Added detailed comments so we can better understand each function

--- src/main/java/com/zipcodewilmington/scientificcalculator/CoreFeatures.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/CoreFeatures.java
@@ -5,30 +5,34 @@ public class CoreFeatures {
     //a state represening the value currently displayed on the calc 
     private double displayValue;
 
-    //Update the display to ERR if an error occurs (divide by 0)
+    //Update the display to ERR if an error occurs (ex: divide by 0)
     private boolean errorState;
 
     //initiate calculator with 0 as default 
     public CoreFeatures () {
         this.displayValue = 0.0;
+        //not an ERR
         this.errorState=false;
 
     }
 
     //Get the current # on the display 
     public double getDisplayValue() {
+        //pull the display value 
         return this.displayValue;
     }
 
     //Check for errors
     public boolean inErrorState() {
+        //pull the error state (is it true or false)
         return this.errorState;
     }
 
     //Set display to the number entered 
-    // block errors 
     public void setDisplay (double value) {
+        //(if ERR, skip the equation)
         if (checkError()) return;
+        //reset the display value to the number entered
         this.displayValue=value;
 
     }
@@ -36,12 +40,12 @@ public class CoreFeatures {
     //Clear the display
       public void clear () {
 
-        //set display to 0 
+        //set display to 0 and remove error
         this.displayValue=0.0;
         this.errorState=false;
 
 
-        // Clear terminal by print lines to simulate clearing
+        // Clear terminal by printing lines to simulate clearing
         for (int i = 0; i < 100; ++i) {
             System.out.println();
         }
@@ -49,26 +53,26 @@ public class CoreFeatures {
       }
     
 
-    //Add
+    //Add (if ERR, skip the equation)
     public void add(double number) {
         if (checkError()) return;
         this.displayValue+=number;
     }
 
 
-    //Subtract
+    //Subtract (if ERR, skip the equation)
     public void subtract (double number) {
         if (checkError()) return;
         this.displayValue -= number;
     }
 
-    //Multiply 
+    //Multiply (if ERR, skip the equation)
     public void multiply (double number) {
         if (checkError()) return;
         this.displayValue *= number;
     }
 
-    //Divide 
+    //Divide (if ERR, skip the equation)
     public void divide(double number) {
         if (checkError()) return;
         if(number ==0) {
@@ -78,13 +82,13 @@ public class CoreFeatures {
         this.displayValue/=number;
     }
 
-    //Caclulate the square 
+    //Caclulate the square (if ERR, skip the equation) 
     public void square () {
         if (checkError()) return;
         this.displayValue=this.displayValue*this.displayValue;
     }
 
-    //Calculate the square root
+    //Calculate the square root (if ERR, skip the equation)
     public void squareRoot() {
         if (checkError()) return;
         if (this.displayValue<0) {
@@ -94,13 +98,13 @@ public class CoreFeatures {
         this.displayValue=Math.sqrt((this.displayValue));
     }
     
-    //Calculate the variable exponentiation 
+    //Calculate the variable exponentiation (if ERR, skip the equation)
     public void exponentiation(double exponent) {
         if (checkError()) return;
         this.displayValue=Math.pow(this.displayValue, exponent);
     }
 
-    //Calculate the inverse of the number
+    //Calculate the inverse of the number (if ERR, skip the equation)
     public void inverse() {
         if (checkError()) return;
         if (this.displayValue==0) {
@@ -111,6 +115,7 @@ public class CoreFeatures {
     }
 
     //Invert the sign of the number (switch between postive and negative)
+    //(if ERR, skip the equation)
     public void inverseSign() {
         if (checkError()) return;
         this.displayValue=-this.displayValue;
@@ -118,10 +123,12 @@ public class CoreFeatures {
     }
 
 
-    //Check and set error
+    //Set an error state so we can use it to compare 
     private void setError() {
         this.errorState=true; 
     }
+
+    //check for an error before operation 
     private boolean checkError() {
         if (this.errorState) {
             System.out.println("ERR");
