28b43282cd0b0dcb62fdd783d09512823ecf7a1c
FrankMontgomery063 <montgomeryfrank.fm@gmail.com>
2025-07-12 15:09:27 -0400

    set options took user input

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -5,13 +5,112 @@ package com.zipcodewilmington.scientificcalculator;
  */
 public class MainApplication {
     public static void main(String[] args) {
-        Console.println("Welcome to my calculator!");
-        String s = Console.getStringInput("Enter a string");
-        Integer i = Console.getIntegerInput("Enter an integer");
-        Double d = Console.getDoubleInput("Enter a double.");
+        System.out.println("\nWelcome to my calculator!\n");
 
-        Console.println("The user input %s as a string", s);
-        Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
+        Double userinput;
+        Integer option=-1;
+
+
+        while(option!=19){
+            userinput = Console.getDoubleInput("Enter a number: ");
+            showOption();
+            option = Console.getIntegerInput("choose one of the options above: ");
+            runOption(option);
+
+        }
+
+
+    }
+
+    public static void showOption(){
+        System.out.println("0. clear display");
+        System.out.println("1. add");
+        System.out.println("2. subtract");
+        System.out.println("3. multiply");
+        System.out.println("4. division");
+        System.out.println("5. square");
+        System.out.println("6. square root");
+        System.out.println("7. inverse");
+        System.out.println("8. switch sign");
+        System.out.println("9. sine");
+        System.out.println("10. cosine");
+        System.out.println("11. tangent");
+        System.out.println("12. inverse sine");
+        System.out.println("13. inverse cosine");
+        System.out.println("14. inverse tangent");
+        System.out.println("15. factorial");
+        System.out.println("16. switch display mode");
+        System.out.println("17. switch units mode");
+        System.out.println("18. memory functions");
+        System.out.println("19. exit");
+    }
+
+    public static void runOption(Integer option){
+        switch (option) {
+            case 0:
+                
+                break;
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+            case 6:
+                
+                break;
+            case 7:
+                
+                break;
+            case 8:
+                
+                break;
+            case 9:
+                
+                break;
+            case 10:
+                
+                break;
+            case 11:
+                
+                break;
+            case 12:
+                
+                break;
+            case 13:
+                
+                break;
+            case 14:
+                
+                break;
+            case 15:
+                
+                break;
+            case 16:
+                
+                break;
+            case 17:
+                
+                break;
+            case 18:
+                
+                break;
+            case 19:
+                
+                break;
+                
+            default:
+
+                break;
+        }
     }
 }
